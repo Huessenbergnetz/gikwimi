f125ed4f93958941ff6c3426d9998e9a5fe55b83
Buschmann <mf@huessenbergnetz.de>
2022-02-09 12:34:00 +0100

    Require libfirfuorida and start implementing gikwimictl app

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -1,1 +1,25 @@
- 
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include <QCoreApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    app.setOrganizationName(QStringLiteral("Huessenbergnetz"));
+    app.setOrganizationDomain(QStringLiteral("huessenbergnetz.de"));
+    app.setApplicationName(QStringLiteral("gikwimictl"));
+    app.setApplicationVersion(QStringLiteral(GIKWIMI_VERSION));
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.process(app);
+
+    return 0;
+}
