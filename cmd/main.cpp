94626a17f4a871fdd52ff7947dea4ad04d8a73df
buschmann23 <mf@huessenbergnetz.de>
2022-04-08 17:19:01 +0200

    [gikwimictl] add option to set config ini file

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -19,37 +19,50 @@ int main(int argc, char *argv[])
     parser.addHelpOption();
     parser.addVersionOption();
 
-    //: CLI option description
-    //% "Runs all database migrations not already applied"
-    QCommandLineOption dbMigrate(QStringLiteral("db-migrate"), qtTrId("gikctl-cliopt-dbmigrate-desc"));
-    parser.addOption(dbMigrate);
+    {
+        const QString iniFileDefVal  = QStringLiteral(GIKWIMI_CONFFILE);
+        QCommandLineOption iniFile(QStringList({QStringLiteral("ini"), QStringLiteral("i")}),
+                                   //: CLI option description, %1 will be the default value
+                                   //% "Path to configuration ini file. Default: %1"
+                                   qtTrId("gikctl-cliopt-inifile-desc").arg(iniFileDefVal),
+                                   //: CLI option value name
+                                   //% "ini file"
+                                   qtTrId("gikctl-cliopt-inifile-val"),
+                                   iniFileDefVal);
+        parser.addOption(iniFile);
 
-    const QString dbRollbackDefVal = QStringLiteral("1");
-    QCommandLineOption dbRollback(QStringLiteral("db-rollback"),
-                                  //: CLI option description, %1 will be the default value
-                                  //% "Rolls back given steps of already applied database migrations. Default: %1"
-                                  qtTrId("gikctl-cliopt-dbrollback-desc").arg(dbRollbackDefVal),
-                                  //: CLI option value name
-                                  //% "steps"
-                                  qtTrId("gikctl-cliopt-dbrollback-val"),
-                                  dbRollbackDefVal);
-    parser.addOption(dbRollback);
+        //: CLI option description
+        //% "Runs all database migrations not already applied"
+        QCommandLineOption dbMigrate(QStringLiteral("db-migrate"), qtTrId("gikctl-cliopt-dbmigrate-desc"));
+        parser.addOption(dbMigrate);
 
-    const QString dbRefreshDefVal = QStringLiteral("0");
-    QCommandLineOption dbRefresh(QStringLiteral("db-refresh"),
-                                 //: CLI option description, %1 will be the default value
-                                 //% Rolls back and reapplies the given number of database migrations. If steps is 0, all migrations will be rolled back and reapplied. Default: %1"
-                                 qtTrId("gikctl-cliopt-dbrefresh-desc").arg(dbRefreshDefVal),
-                                 //: CLI option value name
-                                 //% "steps"
-                                 qtTrId("gikctl-cliopt-dbrefresh-val"),
-                                 dbRefreshDefVal);
-    parser.addOption(dbRefresh);
+        const QString dbRollbackDefVal = QStringLiteral("1");
+        QCommandLineOption dbRollback(QStringLiteral("db-rollback"),
+                                      //: CLI option description, %1 will be the default value
+                                      //% "Rolls back given steps of already applied database migrations. Default: %1"
+                                      qtTrId("gikctl-cliopt-dbrollback-desc").arg(dbRollbackDefVal),
+                                      //: CLI option value name
+                                      //% "steps"
+                                      qtTrId("gikctl-cliopt-dbrollback-val"),
+                                      dbRollbackDefVal);
+        parser.addOption(dbRollback);
 
-    //: CLI option description
-    //% "Resets the database by rolling back all migrations."
-    QCommandLineOption dbReset(QStringLiteral("db-reset"), qtTrId("gikctl-cliopt-dbreset-desc"));
-    parser.addOption(dbReset);
+        const QString dbRefreshDefVal = QStringLiteral("0");
+        QCommandLineOption dbRefresh(QStringLiteral("db-refresh"),
+                                     //: CLI option description, %1 will be the default value
+                                     //% Rolls back and reapplies the given number of database migrations. If steps is 0, all migrations will be rolled back and reapplied. Default: %1"
+                                     qtTrId("gikctl-cliopt-dbrefresh-desc").arg(dbRefreshDefVal),
+                                     //: CLI option value name
+                                     //% "steps"
+                                     qtTrId("gikctl-cliopt-dbrefresh-val"),
+                                     dbRefreshDefVal);
+        parser.addOption(dbRefresh);
+
+        //: CLI option description
+        //% "Resets the database by rolling back all migrations."
+        QCommandLineOption dbReset(QStringLiteral("db-reset"), qtTrId("gikctl-cliopt-dbreset-desc"));
+        parser.addOption(dbReset);
+    }
 
     parser.process(app);
 
