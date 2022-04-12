449a692576d123fb4d8c6de26ee21f1b1ebbd701
Buschmann <mf@huessenbergnetz.de>
2022-04-12 21:32:39 +0200

    [gikwimictl] touch DbMigrations class

---
+++ cmd/dbmigrations.cpp
@@ -0,0 +1,6 @@
+#include "dbmigrations.h"
+
+DbMigrations::DbMigrations()
+{
+
+}
---
+++ cmd/dbmigrations.h
@@ -0,0 +1,12 @@
+#ifndef DBMIGRATIONS_H
+#define DBMIGRATIONS_H
+
+#include "configuration.h"
+
+class DbMigrations : public Configuration
+{
+public:
+    DbMigrations();
+};
+
+#endif // DBMIGRATIONS_H
