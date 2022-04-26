fa133b3605fde23229aef8d16cb7a1d669497ef9
Buschmann <mf@huessenbergnetz.de>
2022-04-26 15:52:43 +0200

    [app] UserAuthStoreSql: load all user data from database

--- app/userauthstoresql.cpp
+++ app/userauthstoresql.cpp
@@ -24,7 +24,7 @@ AuthenticationUser UserAuthStoreSql::findUser(Context *c, const ParamsMultiMap &
 
     const QString username = userinfo.value(QStringLiteral("username"));
 
-    QSqlQuery q = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT u.id, u.type, u.password FROM users u WHERE u.username = :username"));
+    QSqlQuery q = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT id, type, password, email, created_at, updated_at, locked_at, locked_by FROM users WHERE username = :username"));
     q.bindValue(QStringLiteral(":username"), username);
 
     if (Q_LIKELY(q.exec())) {
@@ -33,14 +33,21 @@ AuthenticationUser UserAuthStoreSql::findUser(Context *c, const ParamsMultiMap &
             user.insert(QStringLiteral("username"), username);
             user.insert(QStringLiteral("type"), q.value(1));
             user.insert(QStringLiteral("password"), q.value(2));
+            user.insert(QStringLiteral("email"), q.value(3));
+            user.insert(QStringLiteral("created_at"), q.value(4));
+            user.insert(QStringLiteral("updated_at"), q.value(5));
+            user.insert(QStringLiteral("locked_at"), q.value(6));
+            user.insert(QStringLiteral("locked_by"), q.value(7));
 
             QSqlQuery qq = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT name, value FROM user_settings WHERE user_id = :user_id"));
             qq.bindValue(QStringLiteral(":user_id"), user.id());
 
             if (Q_LIKELY(qq.exec())) {
+                QVariantMap settings;
                 while (Q_LIKELY(qq.next())) {
-                    user.insert(qq.value(0).toString(), qq.value(1));
+                    settings.insert(qq.value(0).toString(), qq.value(1));
                 }
+                user.insert(QStringLiteral("settings"), settings);
             } else {
                 qCCritical(GIK_AUTH) << "Failed to execute database query to get user settings for user" << username << "from the database:" << qq.lastError().text();
             }
