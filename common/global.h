aa6a27b26832ea8a752df0d1834ed95ffabf6966
Buschmann <mf@huessenbergnetz.de>
2022-04-26 12:40:54 +0200

    [general] add global.h to define some global stuff

---
+++ common/global.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef GIKWIMI_GLOBAL_H
+#define GIKWIMI_GLOBAL_H
+
+#include <QtGlobal>
+
+using dbid_t = quint32;
+
+#endif // GIKWIMI_GLOBAL_H
