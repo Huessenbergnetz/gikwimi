3346f165e42051801805fa3cb5ee576d2302a596
Buschmann <mf@huessenbergnetz.de>
2022-05-04 15:30:05 +0200

    [app] add SimpleUser object

---
+++ app/objects/simpleuser.cpp
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "simpleuser.h"
+
+class SimpleUserData : public QSharedData
+{
+public:
+    QString username;
+    dbid_t id = 0;
+};
+
+SimpleUser::SimpleUser()
+{
+
+}
+
+SimpleUser::SimpleUser(dbid_t id, const QString &username)
+    : d(new SimpleUserData)
+{
+    d->username = username;
+    d->id = id;
+}
+
+SimpleUser::SimpleUser(const SimpleUser &other) = default;
+
+SimpleUser::SimpleUser(SimpleUser &&other) noexcept = default;
+
+SimpleUser &SimpleUser::operator=(const SimpleUser &other) = default;
+
+SimpleUser &SimpleUser::operator=(SimpleUser &&other) noexcept = default;
+
+SimpleUser::~SimpleUser() = default;
+
+void SimpleUser::swap(SimpleUser &other)
+{
+    std::swap(d, other.d);
+}
+
+dbid_t SimpleUser::id() const
+{
+    return d ? d->id : 0;
+}
+
+QString SimpleUser::username() const
+{
+    return d ? d->username : QString();
+}
+
+bool SimpleUser::isValid() const
+{
+    return d && d->id > 0;
+}
+
+bool SimpleUser::isNull() const
+{
+    return d ? false : true;
+}
+
+#include "moc_simpleuser.cpp"
---
+++ app/objects/simpleuser.h
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef GIKWIMI_SIMPLEUSER_H
+#define GIKWIMI_SIMPLEUSER_H
+
+#include "global.h"
+
+#include <QObject>
+#include <QSharedDataPointer>
+
+class SimpleUserData;
+
+class SimpleUser
+{
+    Q_GADGET
+    Q_PROPERTY(dbid_t id READ id CONSTANT)
+    Q_PROPERTY(QString username READ username CONSTANT)
+public:
+    SimpleUser();
+
+    SimpleUser(dbid_t id, const QString &username);
+
+    SimpleUser(const SimpleUser &other);
+
+    SimpleUser(SimpleUser &&other) noexcept;
+
+    SimpleUser &operator=(const SimpleUser &other);
+
+    SimpleUser &operator=(SimpleUser &&other) noexcept;
+
+    ~SimpleUser();
+
+    void swap(SimpleUser &other);
+
+    dbid_t id() const;
+
+    QString username() const;
+
+    bool isValid() const;
+
+    bool isNull() const;
+
+private:
+    QSharedDataPointer<SimpleUserData> d;
+};
+
+#endif // GIKWIMI_SIMPLEUSER_H
