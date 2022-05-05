950028e077b7adfb4fca2b333e3409be39e8cfd3
Buschmann <mf@huessenbergnetz.de>
2022-05-05 16:58:45 +0200

    [app] add Guest object

---
+++ app/objects/guest.cpp
@@ -0,0 +1,281 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "guest.h"
+
+#include <Cutelyst/Context>
+#include <Cutelyst/Plugins/Utils/Sql>
+
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QRandomGenerator>
+#include <QMetaObject>
+#include <QMetaEnum>
+
+class GuestData : public QSharedData
+{
+public:
+    Event event;
+    Contact contact;
+    Contact partner;
+    QString partnerGivenName;
+    QString partnerFamilyName;
+    QString note;
+    QString comment;
+    QString uid;
+    QDateTime created;
+    QDateTime updated;
+    QDateTime lockedAt;
+    SimpleUser lockedBy;
+    dbid_t id = 0;
+    quint8 adults = 1;
+    quint8 children = 0;
+    Guest::Status status = Guest::InvalidStatus;
+    Guest::Notifications notifications = Guest::NotNotified;
+};
+
+Guest::Guest()
+{
+
+}
+
+Guest::Guest(const Guest &other) = default;
+
+Guest::Guest(Guest &&other) noexcept = default;
+
+Guest &Guest::operator=(const Guest &other) = default;
+
+Guest &Guest::operator=(Guest &&other) noexcept = default;
+
+Guest::~Guest()
+{
+
+}
+
+dbid_t Guest::id() const
+{
+    return d ? d->id : 0;
+}
+
+QString Guest::uid() const
+{
+    return d ? d->uid : QString();
+}
+
+Event Guest::event() const
+{
+    return d ? d->event : Event();
+}
+
+Contact Guest::contact() const
+{
+    return d ? d->contact : Contact();
+}
+
+QString Guest::partnerGivenName() const
+{
+    if (d) {
+        return d->partner.isNull() ? d->partnerGivenName : d->partner.addressee().givenName();
+    } else {
+        return QString();
+    }
+}
+
+QString Guest::partnerFamilyName() const
+{
+    if (d) {
+        return d->partner.isNull() ? d->partnerFamilyName : d->partner.addressee().familyName();
+    } else {
+        return QString();
+    }
+}
+
+quint8 Guest::adults() const
+{
+    return d ? d->adults : 0;
+}
+
+quint8 Guest::children() const
+{
+    return d ? d->children : 0;
+}
+
+Guest::Status Guest::status() const
+{
+    return d ? d->status : Guest::InvalidStatus;
+}
+
+Guest::Notifications Guest::notifications() const
+{
+    return d ? d->notifications : Guest::NotNotified;
+}
+
+QString Guest::note() const
+{
+    return d ? d->note : QString();
+}
+
+QString Guest::comment() const
+{
+    return d ? d->comment : QString();
+}
+
+QDateTime Guest::created() const
+{
+    return d ? d->created : QDateTime();
+}
+
+QDateTime Guest::updated() const
+{
+    return d ? d->updated : QDateTime();
+}
+
+QDateTime Guest::lockedAt() const
+{
+    return d ? d->lockedAt : QDateTime();
+}
+
+SimpleUser Guest::lockedBy() const
+{
+    return d ? d->lockedBy : SimpleUser();
+}
+
+bool Guest::isValid() const
+{
+    return d && d->id > 0;
+}
+
+bool Guest::isNull() const
+{
+    return d ? false : true;
+}
+
+QString Guest::generateUid()
+{
+    QString uid;
+
+    static int length = 6;
+    static std::vector<int> ascii = ([]() -> std::vector<int> {
+                                         std::vector<int> vec;
+                                         vec.reserve(36);
+                                         // decimal ascii codes for 0-9
+                                         for (int i = 48; i < 58; ++i) {
+                                             vec.push_back(i);
+                                         }
+                                         // decimal ascii codes for a-z
+                                         for (int i = 97; i < 123; ++i) {
+                                             vec.push_back(i);
+                                         }
+                                         return vec;
+                                     }());
+
+    auto rand = QRandomGenerator::global();
+    uid.reserve(6);
+
+    for (int i = 0; i < length; ++i) {
+        uid.append(QChar(rand->bounded(0, ascii.size() - 1)));
+    }
+
+    return uid;
+}
+
+Guest::Status Guest::statusStringToEnum(const QString &str)
+{
+    if (str.compare(QLatin1String("defaultstatus"), Qt::CaseInsensitive) == 0) {
+        return Guest::DefaultStaus;
+    } else if (str.compare(QLatin1String("agreed"), Qt::CaseInsensitive) == 0) {
+        return Guest::Agreed;
+    } else if (str.compare(QLatin1String("canceled"), Qt::CaseInsensitive) == 0) {
+        return Guest::Canceled;
+    } else {
+        return Guest::InvalidStatus;
+    }
+}
+
+QString Guest::statusEnumToString(Guest::Status status)
+{
+    QString str;
+
+    if (status != Guest::InvalidStatus) {
+        const QMetaObject mo = Guest::staticMetaObject;
+        const QMetaEnum   me = mo.enumerator(mo.indexOfEnumerator("Status"));
+        str = QString::fromLatin1(me.valueToKey(static_cast<int>(status)));
+    }
+
+    return str;
+}
+
+QStringList Guest::supportedStatus()
+{
+    QStringList lst;
+
+    const QMetaObject mo = Guest::staticMetaObject;
+    const QMetaEnum   me = mo.enumerator(mo.indexOfEnumerator("Status"));
+
+    lst.reserve(me.keyCount() - 1);
+
+    for (int i = 1; i < me.keyCount(); ++i) {
+        lst.append(QString::fromLatin1(me.key(i)));
+    }
+
+    return lst;
+}
+
+std::vector<Guest> Guest::list(Cutelyst::Context *c, Error &e, const Event &event)
+{
+    std::vector<Guest> guests;
+
+    return guests;
+}
+
+QDataStream &operator<<(QDataStream &stream, const Guest &guest)
+{
+    stream << guest.d->event
+           << guest.d->contact
+           << guest.d->partner
+           << guest.d->partnerGivenName
+           << guest.d->partnerFamilyName
+           << guest.d->note
+           << guest.d->comment
+           << guest.d->uid
+           << guest.d->created
+           << guest.d->updated
+           << guest.d->lockedAt
+           << guest.d->lockedBy
+           << guest.d->id
+           << guest.d->adults
+           << guest.d->children
+           << guest.d->status
+           << guest.d->notifications;
+    return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, Guest &guest)
+{
+    if (guest.d == nullptr) {
+        guest.d = new GuestData;
+    }
+
+    stream >> guest.d->event;
+    stream >> guest.d->contact;
+    stream >> guest.d->partner;
+    stream >> guest.d->partnerGivenName;
+    stream >> guest.d->partnerFamilyName;
+    stream >> guest.d->note;
+    stream >> guest.d->comment;
+    stream >> guest.d->uid;
+    stream >> guest.d->created;
+    stream >> guest.d->updated;
+    stream >> guest.d->lockedAt;
+    stream >> guest.d->lockedBy;
+    stream >> guest.d->id;
+    stream >> guest.d->adults;
+    stream >> guest.d->children;
+    stream >> guest.d->status;
+    stream >> guest.d->notifications;
+    return stream;
+}
+
+#include "moc_guest.cpp"
---
+++ app/objects/guest.h
@@ -0,0 +1,138 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef GIKWIMI_GUEST_H
+#define GIKWIMI_GUEST_H
+
+#include "event.h"
+#include "simpleuser.h"
+#include "global.h"
+#include "contact.h"
+
+#include <QObject>
+#include <QSharedDataPointer>
+
+namespace Cutelyst {
+class Context;
+}
+class Error;
+class GuestData;
+
+class Guest
+{
+    Q_GADGET
+    Q_PROPERTY(dbid_t id READ id CONSTANT)
+    Q_PROPERTY(QString uid READ uid CONSTANT)
+    Q_PROPERTY(Event event READ event CONSTANT)
+    Q_PROPERTY(Contact contact READ contact CONSTANT)
+    Q_PROPERTY(QString partnerGivenName READ partnerGivenName CONSTANT)
+    Q_PROPERTY(QString partnerFamilyName READ partnerFamilyName CONSTANT)
+    Q_PROPERTY(quint8 adults READ adults CONSTANT)
+    Q_PROPERTY(quint8 children READ children CONSTANT)
+    Q_PROPERTY(Guest::Status status READ status CONSTANT)
+    Q_PROPERTY(Guest::Notifications notifications READ notifications CONSTANT)
+    Q_PROPERTY(QString note READ note CONSTANT)
+    Q_PROPERTY(QString comment READ comment CONSTANT)
+    Q_PROPERTY(QDateTime created READ created CONSTANT)
+    Q_PROPERTY(QDateTime updated READ updated CONSTANT)
+    Q_PROPERTY(QDateTime lockedAt READ lockedAt CONSTANT)
+    Q_PROPERTY(SimpleUser lockedBy READ lockedBy CONSTANT)
+public:
+    enum Status : qint8 {
+        InvalidStatus   = -1,
+        DefaultStaus    =  0,
+        Agreed          =  1,
+        Canceled        =  2
+    };
+    Q_ENUM(Status)
+
+    enum Notification : quint8 {
+        NotNotified     = 0x0,
+        Email           = 0x1,
+        SMS             = 0x2,
+        Messenger       = 0x4,
+        Postal          = 0x8
+    };
+    Q_DECLARE_FLAGS(Notifications, Notification)
+    Q_FLAG(Notifications)
+
+    Guest();
+
+    Guest(dbid_t id, const QString &uid, const Event &event, const Contact &contact, const Contact &partner, const QString &pgName, const QString &pfName, quint8 adults, quint8 children, Guest::Status status, Guest::Notifications notifications, const QString &note, const QString &comment);
+
+    Guest(const Guest &other);
+
+    Guest(Guest &&other) noexcept;
+
+    Guest &operator=(const Guest &other);
+
+    Guest &operator=(Guest &&other) noexcept;
+
+    ~Guest();
+
+    void swap(Guest &other) noexcept;
+
+    dbid_t id() const;
+
+    QString uid() const;
+
+    Event event() const;
+
+    Contact contact() const;
+
+    QString partnerGivenName() const;
+
+    QString partnerFamilyName() const;
+
+    quint8 adults() const;
+
+    quint8 children() const;
+
+    Guest::Status status() const;
+
+    Guest::Notifications notifications() const;
+
+    QString note() const;
+
+    QString comment() const;
+
+    QDateTime created() const;
+
+    QDateTime updated() const;
+
+    QDateTime lockedAt() const;
+
+    SimpleUser lockedBy() const;
+
+    bool isValid() const;
+
+    bool isNull() const;
+
+    static QString generateUid();
+
+    static Guest::Status statusStringToEnum(const QString &str);
+
+    static QString statusEnumToString(Guest::Status status);
+
+    static QStringList supportedStatus();
+
+    static std::vector<Guest> list(Cutelyst::Context *c, Error &e, const Event &event);
+
+private:
+    QSharedDataPointer<GuestData> d;
+
+    friend QDataStream &operator<<(QDataStream &stream, const Guest &guest);
+    friend QDataStream &operator>>(QDataStream &stream, Guest &guest);
+};
+
+Q_DECLARE_METATYPE(Guest)
+Q_DECLARE_TYPEINFO(Guest, Q_MOVABLE_TYPE);
+Q_DECLARE_OPERATORS_FOR_FLAGS(Guest::Notifications)
+
+QDataStream &operator<<(QDataStream &stream, const Guest &guest);
+
+QDataStream &operator>>(QDataStream &stream, Guest &guest);
+
+#endif // GIKWIMI_GUEST_H
