07dea48250626112951818bec77473f5c503bd46
Buschmann <mf@huessenbergnetz.de>
2022-05-05 21:50:53 +0200

    [app] add more events and guest controllers

--- app/controllers/controlcenterevents.cpp
+++ app/controllers/controlcenterevents.cpp
@@ -7,6 +7,8 @@
 #include "../objects/user.h"
 #include "../objects/event.h"
 #include "../objects/error.h"
+#include "../objects/guest.h"
+#include "../utils.h"
 
 ControlCenterEvents::ControlCenterEvents(QObject *parent)
     : Controller{parent}
@@ -43,6 +45,59 @@ void ControlCenterEvents::index(Context *c)
                  {QStringLiteral("template"), QStringLiteral("controlcenter/events/index.tmpl")},
                  {QStringLiteral("events"), QVariant::fromValue<std::vector<Event>>(events)},
                  {QStringLiteral("events_table_headers"), QVariant::fromValue<QMap<QString,QString>>(eventsTableHeaders)}
+             });
+}
+
+void ControlCenterEvents::base(Context *c, const QString &id)
+{
+    bool ok = false;
+    const dbid_t eventId = Utils::strToDbid(id, &ok, c->translate("ControlCenterEvents", "Invalid event database ID."), c);
+    if (Q_LIKELY(ok)) {
+        Event::toStash(c, eventId);
+    }
+}
+
+void ControlCenterEvents::edit(Context *c)
+{
+    c->stash({
+                 {QStringLiteral("site_subtitle"), c->translate("ControlCenterEvents", "Edit")},
+                 {QStringLiteral("template"), QStringLiteral("controlcenter/events/edit.tmpl")}
+             });
+}
+
+void ControlCenterEvents::guests(Context *c)
+{
+    const auto event = Event::fromStash(c);
+    Error e;
+    const std::vector<Guest> guests = Guest::list(c, &e, event);
+    if (e.type() != Error::NoError) {
+        e.toStash(c, true);
+        return;
+    }
+
+    QMap<QString,QString> guestsTableHeaders;
+
+    c->stash({
+                 {QStringLiteral("site_subtitle"), c->translate("ControlCenterEvents", "Guests")},
+                 {QStringLiteral("template"), QStringLiteral("controlcenter/events/guests/index.tmpl")},
+                 {QStringLiteral("guests"), QVariant::fromValue<std::vector<Guest>>(guests)},
+                 {QStringLiteral("guests_table_headers"), QVariant::fromValue<QMap<QString,QString>>(guestsTableHeaders)}
+             });
+}
+
+void ControlCenterEvents::addGuest(Context *c)
+{
+    c->stash({
+                 {QStringLiteral("site_subtitle"), c->translate("ControlCenterEvents", "Add guests")},
+                 {QStringLiteral("template"), QStringLiteral("controlcenter/events/guests/add.tmpl")}
+             });
+}
+
+void ControlCenterEvents::editGuest(Context *c, const QString &id)
+{
+    c->stash({
+                 {QStringLiteral("site_subtitle"), c->translate("ControlCenterEvents", "Edit guests")},
+                 {QStringLiteral("template"), QStringLiteral("controlcenter/events/guests/edit.tmpl")}
              });
 }
 
--- app/controllers/controlcenterevents.h
+++ app/controllers/controlcenterevents.h
@@ -21,6 +21,21 @@ public:
     C_ATTR(index, :Path :Args(0))
     void index(Context *);
 
+    C_ATTR(base, :Chained("/") :PathPart("controlcenter/events") :CaptureArgs(1))
+    void base(Context *c, const QString &id);
+
+    C_ATTR(edit, :Chained("base") :PathPart("edit") :Args(0))
+    void edit(Context *c);
+
+    C_ATTR(guests, :Chained("base") :PathPart("guests") :Args(0))
+    void guests(Context *c);
+
+    C_ATTR(addGuest, :Chained("base") :PathPart("guests/add") :Args(0))
+    void addGuest(Context *c);
+
+    C_ATTR(editGuest, :Chained("base") :PathPart("guests/edit") :Args(1))
+    void editGuest(Context *c, const QString &id);
+
     C_ATTR(add, :Path("add") :Args(0))
     void add(Context *c);
 };
