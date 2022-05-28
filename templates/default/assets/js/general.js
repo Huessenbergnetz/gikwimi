f58dd980da058e002dd7ef52f268b2e1ed00eb14
Buschmann <mf@huessenbergnetz.de>
2022-05-28 13:56:55 +0200

    [template] use Bootstrap toast to display errors

--- templates/default/assets/js/general.js
+++ templates/default/assets/js/general.js
@@ -8,3 +8,24 @@ var GikDefTmpl = GikDefTmpl || {};
 GikDefTmpl.newXhrHeaders = function() {
     return new Headers({'X-Requested-With': 'XMLHttpRequest'});
 }
+
+GikDefTmpl.showError = function(title, text) {
+    console.error(title + ": " + text);
+    document.getElementById('messageToastIcon').className = 'bi bi-exclamation-diamond text-danger me-2';
+    document.getElementById('messageToastTitle').textContent = title;
+    document.getElementById('messageToastBody').textContent = text;
+    const toast = bootstrap.Toast.getOrCreateInstance(document.getElementById('messageToast'));
+    toast.show();
+}
+
+GikDefTmpl.updateRangeWithLabel = function()
+{
+    const divs = document.getElementsByClassName('range-with-label');
+    for (let i = 0; i < divs.length; ++i) {
+        const range = divs[i].getElementsByTagName('input')[0];
+        const span = divs[i].getElementsByTagName('label')[0].getElementsByTagName('span')[0];
+        range.addEventListener('input', (e) => {span.textContent = e.target.value});
+    }
+}
+
+GikDefTmpl.updateRangeWithLabel();
