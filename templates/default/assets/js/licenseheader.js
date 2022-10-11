7265c7217460cbf957f030cd3db5c33912799618
Buschmann <mf@huessenbergnetz.de>
2022-10-11 11:26:50 +0200

    [Default Template] update bootstrap to version 5.2.2
    * bootstrap 5.2.1 -> 5.2.2

--- templates/default/assets/js/licenseheader.js
+++ templates/default/assets/js/licenseheader.js
@@ -1,6 +1,6 @@
 /* @license
  * Popper.js v2.11.6 (https://popper.js.org/) by Federico Zivolo and contributors,
- * released under MIT license (https://github.com/floating-ui/floating-ui/blob/v2.10.2/LICENSE.md)
- * Bootstrap v5.2.1 (http://getbootstrap.com) by Twitter Inc. and The Bootstrap Authors,
- * released under MIT license (https://github.com/twbs/bootstrap/blob/v5.1.3/LICENSE).
+ * released under MIT license (https://github.com/floating-ui/floating-ui/blob/v2.11.6/LICENSE.md)
+ * Bootstrap v5.2.2 (http://getbootstrap.com) by Twitter Inc. and The Bootstrap Authors,
+ * released under MIT license (https://github.com/twbs/bootstrap/blob/v5.2.2/LICENSE).
  */
